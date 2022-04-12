844886d6a7f2525dc708371003fe3580b2661671
jkmcode <jakub.kumala@jkmcode.eu>
2022-04-12 17:21:24 +0200

    'add_build'

--- backend/urls.py
+++ backend/urls.py
@@ -9,10 +9,8 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-
-    path('',TemplateView.as_view(template_name='index.html')),
-
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
     path('api/orders/', include('base.urls.order_urls')),
